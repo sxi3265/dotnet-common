56383a0e3f115d5c0b494c2adfb9066aae09bf49
sxi3265 <sxi3265@gmail.com>
2021-06-22 20:02:50 +0800

    增加驼峰命名方法

---
+++ EasyNow.Utility/Extensions/CharExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyNow.Utility.Extensions
+{
+    public static class CharExtensions
+    {
+        public static char ToLower(this char c)
+        {
+#if HAVE_CHAR_TO_LOWER_WITH_CULTURE
+            c = char.ToLower(c, CultureInfo.InvariantCulture);
+#else
+            c = char.ToLowerInvariant(c);
+#endif
+            return c;
+        }
+    }
+}
--- EasyNow.Utility/Extensions/StringExtensions.cs
+++ EasyNow.Utility/Extensions/StringExtensions.cs
@@ -193,6 +193,11 @@ namespace EasyNow.Utility.Extensions
             return null;
         }
 
+        /// <summary>
+        /// 获取sha256值
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static string ToSha256String(this string data)
         {
             var bytes = Encoding.UTF8.GetBytes(data);
@@ -205,6 +210,52 @@ namespace EasyNow.Utility.Extensions
             }
  
             return builder.ToString();
+        }
+
+        /// <summary>
+        /// 驼峰命名
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToCamelCase(this string s)
+        {
+            if (string.IsNullOrEmpty(s) || !char.IsUpper(s[0]))
+            {
+                return s;
+            }
+
+            char[] chars = s.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i == 1 && !char.IsUpper(chars[i]))
+                {
+                    break;
+                }
+
+                bool hasNext = (i + 1 < chars.Length);
+                if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+                {
+                    // if the next character is a space, which is not considered uppercase 
+                    // (otherwise we wouldn't be here...)
+                    // we want to ensure that the following:
+                    // 'FOO bar' is rewritten as 'foo bar', and not as 'foO bar'
+                    // The code was written in such a way that the first word in uppercase
+                    // ends when if finds an uppercase letter followed by a lowercase letter.
+                    // now a ' ' (space, (char)32) is considered not upper
+                    // but in that case we still want our current character to become lowercase
+                    if (char.IsSeparator(chars[i + 1]))
+                    {
+                        chars[i] = chars[i].ToLower();
+                    }
+
+                    break;
+                }
+
+                chars[i] = chars[i].ToLower();
+            }
+
+            return new string(chars);
         }
     }
 }
