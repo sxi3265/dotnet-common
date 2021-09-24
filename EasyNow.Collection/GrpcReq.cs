282004ed6b6e2154f1aa3d93697f8b3994bcac1c
sxi3265 <sxi3265@gmail.com>
2021-09-24 10:12:37 +0800

    grpc增加baseservice

---
+++ EasyNow.Collection/GrpcReq.cs
@@ -0,0 +1,14 @@
+﻿using ProtoBuf;
+
+namespace EasyNow.Collection
+{
+    [ProtoContract]
+    public class GrpcReq<T>
+    {
+        /// <summary>
+        /// 数据
+        /// </summary>
+        [ProtoMember(1, Name = "data")]
+        public T Data { get; set; }
+    }
+}
---
+++ EasyNow.Grpc/BaseService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Autofac;
+using EasyNow.Collection;
+using EasyNow.Dto;
+using EasyNow.Dto.Query;
+using EasyNow.Service;
+using JetBrains.Annotations;
+
+namespace EasyNow.Grpc
+{
+    public abstract class BaseService<TEntity, TResult> where TResult : IIdKeyDto
+    {
+        public ILifetimeScope LifetimeScope { get; set; }
+        protected IRepositoryService<TEntity> RepositoryService => LifetimeScope.Resolve<IRepositoryService<TEntity>>();
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<TResult> Add([NotNull] TResult model)
+        {
+            return RepositoryService.AddAsync(model);
+        }
+
+        /// <summary>
+        /// 批量新增
+        /// </summary>
+        /// <param name="models"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<TResult[]> AddRange([NotNull] TResult[] models)
+        {
+            return RepositoryService.AddRangeAsync(models);
+        }
+
+        /// <summary>
+        /// 查询所有
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<TResult[]> QueryAll([NotNull] QueryAllDto query)
+        {
+            return RepositoryService.QueryAllAsync<TResult>(query);
+        }
+
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<PagedList<TResult>> Query([NotNull] QueryDto query)
+        {
+            return RepositoryService.QueryAsync<TResult>(query);
+        }
+
+        /// <summary>
+        /// 根据id查询
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<TResult> Get(GrpcReq<Guid> req)
+        {
+            return RepositoryService.GetAsync<TResult>(req.Data);
+        }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<TResult> Update([NotNull] TResult model)
+        {
+            return RepositoryService.UpdateAsync(model);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [OperationContract]
+        public virtual Task<bool> Delete([NotNull] GrpcReq<Guid[]> req)
+        {
+            return RepositoryService.DeleteAsync(req.Data);
+        }
+    }
+}
