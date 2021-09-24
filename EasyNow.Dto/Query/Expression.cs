ca8cc44b0b19cb0645c266a666bbb9007744d90b
sxi3265 <sxi3265@gmail.com>
2021-09-24 10:45:23 +0800

    dto expression增加grpc特性

--- EasyNow.Dto/Query/Expression.cs
+++ EasyNow.Dto/Query/Expression.cs
@@ -1,20 +1,26 @@
-﻿namespace EasyNow.Dto.Query
+﻿using ProtoBuf;
+
+namespace EasyNow.Dto.Query
 {
+    [ProtoContract]
     public class Expression
     {
         /// <summary>
         /// 表达式
         /// </summary>
+        [ProtoMember(1,Name = "expressions")]
         public Expression[] Expressions { get; set; }
 
         /// <summary>
         /// 条件
         /// </summary>
+        [ProtoMember(2,Name = "conditions")]
         public Condition[] Conditions { get; set; }
 
         /// <summary>
         /// 操作符
         /// </summary>
+        [ProtoMember(3,Name = "operator")]
         public EExpressionOperator Operator { get; set; }
     }
 }
